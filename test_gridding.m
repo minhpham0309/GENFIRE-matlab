e0b7994257b956b670759b7650cbb4bd67b4e705
minhpham0309 <phminh0309@yahoo.com>
2018-10-19 10:13:45 -0700

    Add files via upload

--- test_gridding.m
+++ test_gridding.m
@@ -1,25 +1,27 @@
+addpath('gridding')
+addpath('nufftall-1.33')
 img1 = imread('data\lena_128.jpg');
 img1 = double(img1); img1 = img1/max(img1(:));
 
 img2 = padarray(img1,[192,192],0,'both');
 supp = img2~=0;
-F = fftshift(fft2(img2));
+F = fftshift(fft2(ifftshift(img2)));
 FF = F(:);
 
-[X,Y] = meshgrid(-255.5:1:255.5,-255.5:1:255.5);
+[X,Y] = meshgrid(-256:1:255,-256:1:255);
 XX = X(:);
 YY = Y(:);
 %%
 %  cos -sin   x
 %  sin  cos   0
 line1 = (1:512)'-256.5;
-n_pj = 80;
+n_pj = 40;
 theta = pi/n_pj;
 XX_sample=[];
 YY_sample=[];
 for i=1:n_pj
-    XX_sample_i = line1*cos((i-1)*theta);
-    YY_sample_i = line1*sin((i-1)*theta);
+    XX_sample_i = line1*cos((i-1)*theta)-0.5;
+    YY_sample_i = line1*sin((i-1)*theta)-0.5;
     XX_sample = [XX_sample;XX_sample_i];
     YY_sample = [YY_sample;YY_sample_i];
 end
@@ -28,10 +30,35 @@ end
 %XX_sample = [XX;XX] + 0.1*normrnd(0,1,[2*512^2,1]);
 %YY_sample = [YY;YY] + 0.1*normrnd(0,1,[2*512^2,1]);
 %%
-F_sample = interp2(X,Y,F,XX_sample,YY_sample);
+F_sample = interp2(X,Y,F,XX_sample,YY_sample,'linear');
+
+%% non uniform fft
+%  dirft2d2(nj,xj,yj,iflag=1,ms,mt,fk);
+XX_sample = mod(XX_sample,512)-256;
+YY_sample = mod(YY_sample,512)-256;
+%%
+xj=[XX;XX_sample]; xj=(xj/max(abs(xj)))*pi;
+yj=[YY;YY_sample]; yj=(yj/max(abs(yj)))*pi;
+fk = [FF;F_sample(:)];
+nj = length(xj);
+
+for k=1:1
+u = nufft2d1(nj,yj,xj,fk,1,1e-6,512,512);
+%figure; img(u)
+u = (real(u)).*ifftshift(supp);
+%u(u<0.1&fftshift(supp)) = max(u(:));
+fk = nufft2d2(nj,yj,xj,-1,1e-6,512,512,u);
+fk(512^2+1:end)=F_sample;
+end
+u=fftshift(u);
+
+figure; img(u,'colormap','gray')
+[min(u(:)),max(u(:))]
+
 %% interpolation by common methods
-F_grid = griddata(XX_sample,YY_sample,F_sample,XX,YY,'natural');
+F_grid = griddata(XX_sample,YY_sample,F_sample,XX,YY,'linear');
 F_grid(isnan(F_grid))=0;
+F_grid = reshape(F_grid,512,512);
 %% interpolation by mean curvature
 [~,~,F_grid] = mincurvi(XX_sample,YY_sample,F_sample,XX,YY);
 %% interpolation by kriging
@@ -46,6 +73,9 @@ end
 mask = reshape(mask,512,512);
 mask = mask~=0;
 figure; img(mask)
+%%
+sum(abs(F_grid(mask) - F(mask)))/sum(abs(F(mask)))
+sum(abs(abs(F_grid(mask)) - abs(F(mask))))/sum(abs(F(mask)))
 %% inverse distance weight
 for i=1:512^2
     x_i = XX(i); y_i = YY(i);
@@ -54,6 +84,9 @@ for i=1:512^2
     F_grid(i) = sum(1./distance_matrix(index_i).*F_sample(index_i)) / sum(1./distance_matrix(index_i));
 end
 %% algorithm
+measuredK = ifftshift(F_grid);
+measuredK_mask = ifftshift(mask);
+supp_ifft = ifftshift(supp);
 obj = rand(512,512,'single');
 u = obj;
 %u = img2;
@@ -62,27 +95,28 @@ u = obj;
 x_cen = floor(512/2);
 y_cen = floor(512/2);
 kernel = (XX1-x_cen).^2 + (YY1-y_cen).^2;
-sigma = 512;
+sigma = 800;
 kernel = exp(-kernel/sigma^2);
 
-for k=1:500
+for k=1:200
     z = fftshift(fftn(u));
-    err = sum(abs(z(mask) - F_grid(mask)))/sum(abs(F_grid(mask)));
+    err = sum(abs(abs(z(mask)) - abs(F_grid(mask))))/sum(abs(F_grid(mask)));
     if mod(k,10)==0, fprintf('%d.error = %f\n',k,err);end
     z(mask) = F_grid(mask) ;%+ 0.5*z(mask);
     u_k = ifftn(ifftshift(z));
     obj = 1.3*u_k - .3*u;
-    obj = max(0,real(obj)).*supp;
+    obj = max(0,real(obj)).*supp_ifft;
     %F_obj = fftshift(fftn(obj)) .* kernel; obj = real(ifftn(ifftshift(F_obj)));
     
     u = obj + .3*(u - u_k);
 end
-figure; img(obj,'colormap','gray'); title('natural');
+obj = fftshift(obj);
+figure; img(obj,'colormap','gray'); title('cubic');
 %%
 F_grid = reshape(F_grid,[512,512]);
 F_grid(isnan(F_grid))=0;
 figure;img3 = ifft2(F_grid);
 img(img3);title('nearest');
 %%
-figure; img((abs((reshape(F_grid,512,512))))); colorbar
-figure; img(((F)));colorbar
+figure; img((abs((reshape(F_grid,512,512)))));title('gridding'); colorbar
+figure; img(((F)));colorbar; title('original');
